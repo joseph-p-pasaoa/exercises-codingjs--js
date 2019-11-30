51e3a22a27d9bec00ac7653e3cd05fd043ce11f4
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-30 00:09:16 -0500

    3 more warmup2 exercises completed

--- warmup2.js
+++ warmup2.js
@@ -25,3 +25,59 @@ stringTimes('code', 2) // → codecode	codecode	✔
 stringTimes('code', 3) // → codecodecode	codecodecode	✔
 
 
+function frontTimes(str,n){
+  let output = "";
+  for (let i = n; i > 0; i--) {
+    output += str.slice(0, 3);
+  }
+  return output;
+}
+frontTimes('Chocolate', 2) // → ChoCho	ChoCho	✔	
+frontTimes('Chocolate', 3) // → ChoChoCho	ChoChoCho	✔	
+frontTimes('Abc', 3) // → AbcAbcAbc	AbcAbcAbc	✔	
+frontTimes('Ab', 4) // → AbAbAbAb	AbAbAbAb	✔	
+frontTimes('A', 4) // → AAAA	AAAA	✔	
+frontTimes('',4) // →		✔	
+frontTimes('Abc', 0) // →		✔
+
+
+function countXX(str){
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.slice(i, i + 2) === "xx") {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+countXX('abcxx') // → 1	1	✔	
+countXX('xxx') // → 2	2	✔	
+countXX('xxxx') // → 3	3	✔	
+countXX('abc') // → 0	0	✔	
+countXX('Hello There') // → 0	0	✔	
+countXX('Hexxo Thxxe') // → 2	2	✔	
+countXX('') // → 0	0	✔	
+countXX('Kittens') // → 0	0	✔	
+countXX('Kittensxxx') // → 2	2	✔
+
+
+function doubleX(str){
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x") {
+      return str[i + 1] === "x";
+    }
+  }
+  return false;
+}
+doubleX('axxbb') // → true	true	✔	
+doubleX('axaxax') // → false	false	✔	
+doubleX('xxxxx') // → true	true	✔	
+doubleX('xaxxx') // → false	false	✔	
+doubleX('aaaax') // → false	false	✔	
+doubleX('') // → false	false	✔	
+doubleX('abc') // → false	false	✔	
+doubleX('x') // → false	false	✔	
+doubleX('xx') // → true	true	✔	
+doubleX('xaxx') // → false	false	✔
+
+
