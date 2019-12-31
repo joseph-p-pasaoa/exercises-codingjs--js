d1f28ba160d1663fe2ab111861f5d171f74d191a
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-12-30 23:53:08 -0500

    3 more array1 exercises completed

--- array1.js
+++ array1.js
@@ -44,3 +44,34 @@ function makePi(){
 makePi() // → 3,1,4	3,1,4	✔
 
 
+function commonEnd(a,b){
+  return a[0] === b[0] || a[a.length - 1] === b[b.length - 1];
+}
+commonEnd([1, 2, 3], [7, 3]) // → true	true	✔	
+commonEnd([1, 2, 3], [7, 3, 2]) // → false	false	✔	
+commonEnd([1, 2, 3], [1, 3]) // → true	true	✔	
+commonEnd([1, 2, 3], [1]) // → true	true	✔	
+commonEnd([1, 2, 3], [2]) // → false	false	✔
+
+
+function sum3(nums){
+  return nums[0] + nums[1] + nums[2];
+}
+sum3([1, 2, 3]) // → 6	6	✔	
+sum3([5, 11, 2]) // → 18	18	✔	
+sum3([7, 0, 0]) // → 7	7	✔	
+sum3([1, 2, 1]) // → 4	4	✔	
+sum3([1, 1, 1]) // → 3	3	✔	
+sum3([2, 7, 2]) // → 11	11	✔
+
+
+function rotateLeft3(nums){
+  return [nums[1], nums[2], nums[0]];
+}
+rotateLeft3([1, 2, 3]) // → 2,3,1	2,3,1	✔	
+rotateLeft3([5, 11, 9]) // → 11,9,5	11,9,5	✔	
+rotateLeft3([7, 0, 0]) // → 0,0,7	0,0,7	✔	
+rotateLeft3([1, 2, 1]) // → 2,1,1	2,1,1	✔	
+rotateLeft3([0, 0, 1]) // → 0,1,0	0,1,0	✔
+
+
