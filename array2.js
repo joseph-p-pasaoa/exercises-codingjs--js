5761e9713df7b2150c271f81f2db947d964e1eda
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-25 22:07:22 -0500

    5 more array2 exercises completed

--- array2.js
+++ array2.js
@@ -100,3 +100,127 @@ centeredAverage([4, 4, 4, 1, 5]) // → 4	4	✔
 centeredAverage([6, 4, 8, 12, 3]) // → 6	6	✔
 
 
+function sum13(nums){
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 13) {
+      i += 1;
+    } else {
+      sum += nums[i];
+    }
+  }
+  return sum;
+}
+sum13([1, 2, 2, 1]) // → 6	6	✔	
+sum13([1, 1]) // → 2	2	✔	
+sum13([1, 2, 2, 1, 13]) // → 6	6	✔	
+sum13([1, 2, 13, 2, 1, 13]) // → 4	4	✔	
+sum13([13, 1, 2, 13, 2, 1, 13]) // → 3	3	✔	
+sum13([]) // → 0	0	✔	
+sum13([13]) // → 0	0	✔	
+sum13([13, 13]) // → 0	0	✔	
+sum13([13, 0, 13]) // → 0	0	✔	
+sum13([13, 1, 13]) // → 0	0	✔	
+sum13([5, 7, 2]) // → 14	14	✔	
+sum13([5, 13, 2]) // → 5	5	✔	
+sum13([0]) // → 0	0	✔	
+sum13([13, 0]) // → 0	0	✔
+
+
+function sum67(nums){
+  let amAdding = true;
+  let sum = 0;
+  for (let num of nums) {
+    if (amAdding) {
+      if (num.toString()[0] === "6") {
+        amAdding = false;
+      } else {
+        sum += num;
+      }
+    } else {
+      if (num.toString().includes("7")) {
+        amAdding = true;
+      }
+    }
+  }
+  return sum;
+}
+sum67([1, 2, 2]) // → 5	5	✔	
+sum67([1, 2, 2, 6, 99, 99, 7]) // → 5	5	✔	
+sum67([1, 1, 6, 7, 2]) // → 4	4	✔	
+sum67([1, 6, 2, 2, 7, 1, 6, 99, 99, 7]) // → 2	2	✔	
+sum67([1, 6, 2, 6, 2, 7, 1, 6, 99, 99, 7]) // → 2	2	✔	
+sum67([2, 7, 6, 2, 6, 7, 2, 7]) // → 18	18	✔	
+sum67([2, 7, 6, 2, 6, 2, 7]) // → 9	9	✔	
+sum67([1, 6, 7, 7]) // → 8	8	✔	
+sum67([6, 7, 1, 6, 7, 7]) // → 8	8	✔	
+sum67([6, 8, 1, 6, 7]) // → 0	0	✔	
+sum67([]) // → 0	0	✔	
+sum67([6, 7, 11]) // → 11	11	✔	
+sum67([11, 6, 7, 11]) // → 22	22	✔	
+sum67([2, 2, 6, 7, 7]) // → 11	11	✔
+
+
+function has22(nums){
+  for (let [i, num] of nums.entries()) {
+    if (num === 2 && nums[i + 1] === 2) {
+      return true;
+    }
+  }
+  return false;
+}
+has22([1, 2, 2]) // → true	true	✔	
+has22([1, 2, 1, 2]) // → false	false	✔	
+has22([2, 1, 2]) // → false	false	✔	
+has22([2, 2, 1, 2]) // → true	true	✔	
+has22([1, 3, 2]) // → false	false	✔	
+has22([1, 3, 2, 2]) // → true	true	✔	
+has22([2, 3, 2, 2]) // → true	true	✔	
+has22([4, 2, 4, 2, 2, 5]) // → true	true	✔	
+has22([1, 2]) // → false	false	✔	
+has22([2, 2]) // → true	true	✔	
+has22([2]) // → false	false	✔	
+has22([]) // → false	false	✔	
+has22([3, 3, 2, 2]) // → true	true	✔	
+has22([5, 2, 5, 2]) // → false	false	✔
+
+
+function lucky13(nums){
+  return nums.every(num => num !== 1 && num !== 3);
+}
+lucky13([0, 2, 4]) // → true	true	✔	
+lucky13([1, 2, 3]) // → false	false	✔	
+lucky13([1, 2, 4]) // → false	false	✔	
+lucky13([2, 7, 2, 8]) // → true	true	✔	
+lucky13([2, 7, 1, 8]) // → false	false	✔	
+lucky13([3, 7, 2, 8]) // → false	false	✔	
+lucky13([2, 7, 2, 1]) // → false	false	✔	
+lucky13([1, 2]) // → false	false	✔	
+lucky13([2, 2]) // → true	true	✔	
+lucky13([2]) // → true	true	✔	
+lucky13([3]) // → false	false	✔	
+lucky13([]) // → true	true	✔
+
+
+function sum28(nums){
+  return !nums.reduce((strikes, curr) => {
+    if (curr === 2) {
+      return strikes -= 1;
+    }
+    return strikes;
+  }, 4);
+}
+sum28([2, 3, 2, 2, 4, 2]) // → true	true	✔	
+sum28([2, 3, 2, 2, 4, 2, 2]) // → false	false	✔	
+sum28([1, 2, 3, 4]) // → false	false	✔	
+sum28([2, 2, 2, 2]) // → true	true	✔	
+sum28([1, 2, 2, 2, 2, 4]) // → true	true	✔	
+sum28([]) // → false	false	✔	
+sum28([2]) // → false	false	✔	
+sum28([8]) // → false	false	✔	
+sum28([2, 2, 2]) // → false	false	✔	
+sum28([2, 2, 2, 2, 2]) // → false	false	✔	
+sum28([1, 2, 2, 1, 2, 2]) // → true	true	✔	
+sum28([5, 2, 2, 2, 4, 2]) // → true	true	✔
+
+
