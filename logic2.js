eebb17d5d9c76e6c24ff7901225e4183368aa53a
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-07-07 22:20:12 -0400

    completed 1 more logic2 exercise

--- logic2.js
+++ logic2.js
@@ -122,3 +122,39 @@ noTeenSum(17, 18, 16) // → 16	16	✔
 noTeenSum(17, 19, 18) // → 0	0	✔
 
 
+/*
+-- blackjack
+Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. Return 0 if they both go over.
+
+EXAMPLES
+blackjack(19, 21) → 21
+blackjack(21, 19) → 21
+blackjack(19, 22) → 19
+*/
+
+function blackjack(a, b){
+  if (a > 21 && b > 21) return 0;
+  if (a > 21) return b;
+  if (b > 21) return a;
+
+  const aTo21 = 21 - a;
+  const bTo21 = 21 - b;
+  return aTo21 < bTo21 ? a : b;
+}
+blackjack(19, 21) // → 21	21	✔
+blackjack(21, 19) // → 21	21	✔
+blackjack(19, 22) // → 19	19	✔
+blackjack(22, 19) // → 19	19	✔
+blackjack(22, 50) // → 0	0	✔
+blackjack(22, 22) // → 0	0	✔
+blackjack(33, 1) // → 1	1	✔
+blackjack(1, 2) // → 2	2	✔
+blackjack(34, 33) // → 0	0	✔
+blackjack(17, 19) // → 19	19	✔
+blackjack(18, 17) // → 18	18	✔
+blackjack(16, 23) // → 16	16	✔
+blackjack(3, 4) // → 4	4	✔
+blackjack(3, 2) // → 3	3	✔
+blackjack(21, 20) // → 21	21	✔
+
+
