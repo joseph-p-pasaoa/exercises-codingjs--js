436d6069e08a244816bc7996f7b939eec8a08fb5
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-11-13 11:51:05 -0500

    fix format of warmup1 exercises, prep for instructions adding

--- warmup1.js
+++ warmup1.js
@@ -1,18 +1,16 @@
 /* 
 Joseph P. Pasaoa
 JS Practice. Exercises found at https://codingjs.wmcicompsci.ca/
+WARMUP-1
 */
 
 
 
-//
-// Warmup-1 //
-//
-
 /* SLEEPIN
 The parameter weekday is true if it is a weekday, and the parameter
 vacation is true if we are on vacation. We sleep in if it is not a
-weekday or we're on vacation. Return true if we sleep in. */
+weekday or we're on vacation. Return true if we sleep in.
+*/
 
 function sleepIn(weekday,vacation){
   return !weekday || vacation;
@@ -27,7 +25,8 @@ sleepIn(false, false); // → true	true	✔
 /* MONKEYTROUBLE
 We have two monkeys, a and b, and the parameters aSmile and bSmile 
 indicate if each is smiling. We are in trouble if they are both smiling 
-or if neither of them is smiling. Return true if we are in trouble. */
+or if neither of them is smiling. Return true if we are in trouble.
+*/
 
 function monkeyTrouble(aSmile,bSmile){
   return aSmile === bSmile;
@@ -41,7 +40,8 @@ monkeyTrouble(false, true); // → false	false	✔
 
 /* SUMDOUBLE
 Given two int values, return their sum. Unless the two values are the same, 
-then return double their sum. */
+then return double their sum.
+*/
 
 function sumDouble(a,b){
   return (a === b
@@ -62,7 +62,8 @@ sumDouble(3, 4); // → 7	7	✔
 
 
 /* DIFF21
-Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21. */
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+*/
 
 function diff21(n){
   let aDiff = Math.abs(21 - n);
@@ -88,7 +89,8 @@ diff21(50); // → 58	58	✔
 
 /* PARROTTROUBLE
 We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
-We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble. */
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+*/
 
 function parrotTrouble(talking,hour){
   if (talking) {
@@ -109,7 +111,8 @@ parrotTrouble(false, 12); // → false	false	✔
 
 
 /* MAKES10
-Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10. */
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+*/
 
 function makes10(a,b){
   return a === 10 || b === 10 || a + b === 10;
@@ -128,7 +131,8 @@ makes10(12, -2); // → true	true	✔
 
 /* NEARHUNDRED
 Given an int n, return true if it is within 10 of 100 or 200. 
-Note: Math.abs(num) computes the absolute value of a number. */
+Note: Math.abs(num) computes the absolute value of a number.
+*/
 
 function nearHundred(n){
   let a = Math.abs(100 - n) <= 10;
@@ -151,7 +155,8 @@ nearHundred(189); // → false	false	✔
 /* POSNEG
 Given 2 int values, return true if one is negative and one is positive. 
 Except if the parameter "negative" is true, then return true only if 
-both are negative. */
+both are negative.
+*/
 
 function posNeg(a,b,negative){
   if (negative) {
@@ -184,7 +189,8 @@ posNeg(-5, -5, true); // → true	true	✔
 /* NOTSTRING
 Given a string, return a new string where "not " has been added to the 
 front. However, if the string already begins with "not", return the string 
-unchanged. */
+unchanged.
+*/
 
 function notString(str){
   if (str.slice(0, 3) !== 'not') {
@@ -205,7 +211,8 @@ notString('no'); // → not no	not no	✔
 /* MISSINGCHAR
 Given a non-empty string and an int n, return a new string where the char at
 index n has been removed. The value of n will be a valid index of a char in the
-original string (i.e. n will be in the range 0..str.length()-1 inclusive). */
+original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+*/
 
 function missingChar(str,n){
   let arr = str.split('').splice(n, 1);
@@ -226,7 +233,8 @@ missingChar('chocolate', 8); // → chocolat	chocolat	✔
 
 /* FRONTBACK
 Given a string, return a new string where the first and last chars have been
-exchanged. */
+exchanged.
+*/
 
 function frontBack(str){
   const output = str.split('');
@@ -254,7 +262,8 @@ frontBack('hello') // → oellh	oellh	✔
 /* FRONT3
 Given a string, we'll say that the front is the first 3 chars of the string. If
 the string length is less than 3, the front is whatever is there. Return a new
-string which is 3 copies of the front. */
+string which is 3 copies of the front.
+*/
 
 function front3(str){
   const firstThree = str.slice(0, 3);
@@ -274,7 +283,8 @@ front3('') // →		✔
 /* BACKAROUND
 Given a string, take the last char and return a new string with the last char
 added at the front and back, so 'cat' yields 'tcatt' The original string will
-be length 1 or more. */
+be length 1 or more.
+*/
 
 function backAround(str){
   const lastChar = str[str.length - 1];
@@ -292,7 +302,8 @@ backAround('boo') // → obooo	obooo	✔
 
 /* OR35
 Return true if the given non-negative number is a multiple of 3 or a multiple
-of 5. (Hint: Use the % 'mod' operator) */
+of 5. (Hint: Use the % 'mod' operator)
+*/
 
 function or35(n){
   return n % 3 === 0 || n % 5 === 0;
@@ -326,7 +337,8 @@ or35(123) // → true	true	✔
 /* FRONT22
 Given a string, take the first 2 chars and return the string with the 2 chars
 added at both the front and back, so 'kitten' yields 'kikittenki' chars are
-there. */
+there.
+*/
 
 function front22(str){
   const firstTwo = str.slice(0, 2);
@@ -424,6 +436,11 @@ hasTeen(11, 22, 22) // → false	false	✔
 
 
 
+/* LONETEEN
+We'll say that a number is 'teen' if it is in the range 13..19 inclusive. Given 2 int values,
+return true if one or the other is teen, but not both.
+*/
+
 function loneTeen(a,b){
   const yesA = a >= 13 && a <= 19;
   const yesB = b >= 13 && b <= 19;
@@ -451,6 +468,9 @@ loneTeen(99, 99) // → false	false	✔
 
 
 
+/*
+*/
+
 function delDel(str){
   if (str.slice(1, 4) === "del") {
     return str[0] + str.slice(4);
@@ -470,6 +490,9 @@ delDel('aadelbb') // → aadelbb	aadelbb	✔
 
 
 
+/*
+*/
+
 function mixStart(str){
   return str.slice(1, 3) === "ix";
 }
@@ -481,6 +504,9 @@ mixStart('ni') // → false	false	✔
 mixStart('n') // → false	false	✔
 
 
+
+/*
+*/
 
 function startOz(str){
   return (str[0] === "o" ? "o" : "") + (str[1] === "z" ? "z" : "");
@@ -498,6 +524,9 @@ startOz('zzzz') // → z	z	✔
 startOz('oznic') // → oz	oz	✔
 
 
+
+/*
+*/
 
 function intMax(a,b,c){
   if (a > b) {
@@ -519,6 +548,9 @@ intMax(5, 2, 6) // → 6	6	✔
 
 
 
+/*
+*/
+
 function close10(a,b){
   if ((a + b) / 2 === 10) {
     return 0;
@@ -539,6 +571,9 @@ close10(0, 20) // → 0	0	✔
 close10(10, 10) // → 0	0	✔
 
 
+
+/*
+*/
 
 function in3050(a,b){
   if (a === 40) {
@@ -567,6 +602,9 @@ in3050(35, 45) // → false	false	✔
 
 
 
+/*
+*/
+
 function max1020(a,b){
   if (a >= 10 && a <= 20) {
     if (b >= 10 && b <= 20) {
@@ -593,6 +631,9 @@ max1020(17, 16) // → 17	17	✔
 
 
 
+/*
+*/
+
 function stringE(str){
   let counter = 3;
   for (let char of str) {
@@ -614,6 +655,9 @@ stringE('') // → false	false	✔
 
 
 
+/*
+*/
+
 function lastDigit(a,b){
   return a % 10 === b % 10;
 }
@@ -627,6 +671,9 @@ lastDigit(11, 0) // → false	false	✔
 
 
 
+/*
+*/
+
 function endUp(str){
   return str.slice(0, -3) + str.slice(-3).toUpperCase();
 }
@@ -639,6 +686,9 @@ endUp('x') // → X	X	✔
 endUp('') // →		✔
 
 
+
+/*
+*/
 
 function everyNth(str,n){
   let output = "";
