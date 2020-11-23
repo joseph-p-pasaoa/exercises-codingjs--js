21513564d29b6bbe0baccdda3c727f63f68be4eb
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-11-22 22:22:29 -0500

    complete 1 more map2 exercise

--- map2.js
+++ map2.js
@@ -175,3 +175,48 @@ wordMultiple(["this", "and", "this"]) // → {'this': true, 'and': false}	{'this
 wordMultiple(["d", "a", "e", "d", "a", "d", "b", "b", "z", "a", "a", "b", "z", "x"]) // → {'d': true, 'a': true, 'e': false, 'b': true, 'z': true, 'x': false}	{'d': true, 'a': true, 'e': false, 'b': true, 'z': true, 'x': false}	✔
 
 
+/* ALLSWAP
+We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then
+return the given array of non-empty strings as follows: if a string matches an earlier string in the array,
+swap the 2 strings in the array. When a position in the array has been swapped, it no longer matches anything.
+Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+*/
+
+function allSwap(wordArray){
+  const output = [];
+  const firstsMap = new Map();
+  for (let i = 0; i < wordArray.length; i++) {
+    const [currWord, leadChar] = [
+      wordArray[i],
+      wordArray[i][0]
+    ];
+
+    if (!firstsMap.has(leadChar)) {
+      const wordInfo = {
+        index: i,
+        word: currWord
+      };
+      firstsMap.set(leadChar, wordInfo);
+      output.push(currWord);
+    } else {
+      const past = firstsMap.get(leadChar);
+      firstsMap.delete(leadChar);
+      output[past.index] = currWord;
+      output.push(past.word);
+    }
+  }
+
+  return output;
+}
+allSwap(["ab", "ac"]) // → ac,ab	ac,ab	✔	
+allSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) // → ay,by,cy,cx,bx,ax,azz,aaa	ay,by,cy,cx,bx,ax,azz,aaa	✔	
+allSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) // → ay,by,ax,bx,aj,ai,by,bx	ay,by,ax,bx,aj,ai,by,bx	✔	
+allSwap(["ax", "bx", "cx", "ay", "cy", "aaa", "abb"]) // → ay,bx,cy,ax,cx,abb,aaa	ay,bx,cy,ax,cx,abb,aaa	✔	
+allSwap(["easy", "does", "it", "every", "ice", "eaten"]) // → every,does,ice,easy,it,eaten	every,does,ice,easy,it,eaten	✔	
+allSwap(["list", "of", "words", "swims", "over", "lily", "water", "wait"]) // → lily,over,water,swims,of,list,words,wait	lily,over,water,swims,of,list,words,wait	✔	
+allSwap(["4", "8", "15", "16", "23", "42"]) // → 42,8,16,15,23,4	42,8,16,15,23,4	✔	
+allSwap(["aaa"]) // → aaa	aaa	✔	
+allSwap([]) // →		✔	
+allSwap(["a", "b", "c", "xx", "yy", "zz"]) // → a,b,c,xx,yy,zz	a,b,c,xx,yy,zz	✔
+
+
